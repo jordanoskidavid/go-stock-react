5d720dd6ca58469ce51ce98e88dd3e28511a13fe
jordanoskidavid <jordanoski.david16@gmail.com>
2025-07-13 17:47:54 +0200

    Add config to docker with local go image and mysql latest image.

---
+++ gostock/go.mod
@@ -0,0 +1,3 @@
+module github.com/jordanoskidavid/go-stock-react
+
+go 1.24.4
--- gostock/main.go
+++ gostock/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello David")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintln(w, "Backend with Go is set")
+		if err != nil {
+			return
+		}
+	})
+	fmt.Println("Serverot raboti na ovaa porta http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
