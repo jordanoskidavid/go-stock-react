a51a4abbc8b336e5f68b4a6aa36fae33180caeb8
jordanoskidavid <jordanoski.david16@gmail.com>
2025-07-17 22:08:58 +0200

    Add filters for fetching all users and user by id.

---
+++ gostock/models/UserResponse.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type UserResponse struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
