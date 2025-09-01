5cf8c244ebddeeea9a3f3009b3360d9ebc5219ce
jordanoskidavid <jordanoski.david16@gmail.com>
2025-09-01 18:30:34 +0200

    Add home header component without breadcrumbs.

---
+++ gostock-fe/src/components/pages/home/HeaderHome.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+
+const HeaderHome = () => {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                px: 3,
+                py: 2,
+            }}
+        >
+            {/* Left: Logo */}
+            <Box sx={{ flexShrink: 0 }}>
+                <Link to="/">
+                    <img
+                        src="/logo.png"
+                        alt="logo"
+                        style={{ width: "110px", height: "110px", cursor: "pointer" }}
+                    />
+                </Link>
+            </Box>
+
+            {/* Middle: Hello David */}
+            <Box sx={{ flex: 1, textAlign: "center" }}>
+                <Typography variant="h3">
+                    Hello David!
+                </Typography>
+            </Box>
+
+            {/* Right: Profile Picture */}
+            <Box sx={{ flexShrink: 0 }}>
+                <Link to="/profile">
+                    <img
+                        src="/profilePicture.png"
+                        alt="profile"
+                        style={{ width: "68px", height: "60px", cursor: "pointer" }}
+                    />
+                </Link>
+            </Box>
+        </Box>
+    );
+};
+
+export default HeaderHome;
