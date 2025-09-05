b98d6fa283ad4d9122bf932d2539650931403f74
jordanoskidavid <jordanoski.david16@gmail.com>
2025-09-05 15:17:37 +0200

    Add the structure of the categories.

---
+++ gostock-fe/src/components/pages/categories/HeaderCategory.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+
+const HeaderCategory = () => {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                px: 2,
+                backgroundColor: "#002A41",
+            }}
+        >
+            <Box sx={{ flexShrink: 0 }}>
+                <Link to="/">
+                    <img
+                        src="src/assets/backArrow.png"
+                        alt="logo"
+                        style={{ width: "60px", height: "60px", cursor: "pointer" }}
+                    />
+                </Link>
+            </Box>
+
+            <Box sx={{ flex: 1, textAlign: "center" }}>
+                <Typography variant="h4" sx={{ color:"#e3f2fd"}}>
+                    Categories
+                </Typography>
+            </Box>
+
+            <Box sx={{ flexShrink: 0 }}>
+                <Link to="/">
+                    <img
+                        src="/logo.png"
+                        alt="logo"
+                        style={{ width: "110px", height: "110px", cursor: "pointer" }}
+                    />
+                </Link>
+            </Box>
+        </Box>
+    );
+};
+
+export default HeaderCategory;
